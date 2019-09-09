89d3b5c796f4d95558d4a5afdeefa47a4740673c
Hillary <uptonh13@gmail.com>
2019-09-08 20:19:03 -0400

    Set up connection folder and connect to routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const pool = require("../connection/connection");
 
 router.get("/to_do_and_events", (req, res) => {
   res.send("GET WORKS!!!");
