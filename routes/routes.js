59099f0be2303bd5217e75c00472fc55e2afe35e
Hillary <uptonh13@gmail.com>
2019-09-09 09:42:49 -0400

    Put works

--- routes/routes.js
+++ routes/routes.js
@@ -29,25 +29,25 @@ router.post("/to_do_and_events", (req, res) => {
     });
 });
 
-// router.put("/to_do_and_events/:id", (req, res) => {
-//   pool
-//     .query(
-//       "update to_do_and_events set to_do_item=$1::text, to_do_date=$2::date, to_do_priority=$3::text, event_name=$4::text, event_address=$5::text, event_time=$6::time, event_date=$7::date where id=$8::int",
-//       [
-//         req.body.to_do_item,
-//         req.body.to_do_date,
-//         req.body.to_do_priority,
-//         req.body.event_name,
-//         req.body.event_address,
-//         req.body.event_time,
-//         req.body.event_date,
-//         req.params.id
-//       ]
-//     )
-//     .then(() => {
-//       selectAllList(req, res);
-//     });
-// });
+router.put("/to_do_and_events/:id", (req, res) => {
+  pool
+    .query(
+      "update to_do_and_events set to_do_item=$1::text, to_do_date=$2::date, to_do_priority=$3::text, event_name=$4::text, event_address=$5::text, event_time=$6::time, event_date=$7::date where id=$8::int",
+      [
+        req.body.to_do_item,
+        req.body.to_do_date,
+        req.body.to_do_priority,
+        req.body.event_name,
+        req.body.event_address,
+        req.body.event_time,
+        req.body.event_date,
+        req.params.id
+      ]
+    )
+    .then(() => {
+      selectAllList(req, res);
+    });
+});
 
 // router.delete("/to_do_and_events/1", (req, res) => {
 //   pool
