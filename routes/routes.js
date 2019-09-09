e039eeb2947ec8822b0eb5917a4ddcae9d458072
Hillary <uptonh13@gmail.com>
2019-09-08 20:16:05 -0400

    Post, put, and delete work, add in connection folder

--- routes/routes.js
+++ routes/routes.js
@@ -5,4 +5,16 @@ router.get("/to_do_and_events", (req, res) => {
   res.send("GET WORKS!!!");
 });
 
+router.post("/to_do_and_events", (req, res) => {
+  res.send("POST WORKS!!!");
+});
+
+router.put("/to_do_and_events/:id", (req, res) => {
+  res.send("PUT WORKS!!!");
+});
+
+router.delete("/to_do_and_events/1", (req, res) => {
+  res.send("DELETE WORKS!!!");
+});
+
 module.exports = router;
