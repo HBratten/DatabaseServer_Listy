f70540e188534de1a1343d7146405ac35cff9d87
Hillary <uptonh13@gmail.com>
2019-09-10 15:08:56 -0400

    Event posts to database

--- routes/event.api.js
+++ routes/event.api.js
@@ -11,32 +11,54 @@ function selectAllEvents(req, res) {
 eventRouter.get("/event", selectAllEvents);
 
 eventRouter.post("/event", (req, res) => {
+  const {
+    event_name,
+    event_address,
+    event_date,
+    event_notes,
+    starting_address,
+    starting_city,
+    starting_state,
+    starting_zip,
+    event_city,
+    event_state,
+    event_zip,
+    event_end_time,
+    event_start_time
+  } = req.body;
+// above is destructuring and assigning them to variable names
+
   pool
     .query(
-      "insert into event (event_name, event_address, event_time, event_date, event_notes) values ($1::text, $2::text, $3::time, $4::date, $5::text)",
-      [
-        req.body.event_name,
-        req.body.event_address,
-        req.body.event_time,
-        req.body.event_date,
-        req.body.event_notes
-      ]
+      `insert into event 
+      (event_name, event_address, event_date, event_notes, starting_address, starting_city, starting_state, starting_zip, event_city, event_state, event_zip, event_end_time, event_start_time) 
+      values 
+      ('${event_name}', '${event_address}', '${event_date}', '${event_notes}', '${starting_address}', '${starting_city}', '${starting_state}', ${starting_zip}, '${event_city}', '${event_state}', ${event_zip}, '${event_end_time}', '${event_start_time}')`
     )
     .then(() => {
       selectAllEvents(req, res);
-    });
+    })
+    .catch(e => res.json(e));
 });
 
 eventRouter.put("/event/:id", (req, res) => {
   pool
     .query(
-      "update event set event_name=$1::text, event_address=$2::text, event_time=$3::time, event_date=$4::date, event_notes=$5::text where id=$6::int",
+      "event_name=$1::text, event_address=$2::text, event_date=$3::date, event_notes=$4::text, starting_address=$5::text, starting_city=$6::text, starting_state=$7::text, starting_zip=$8::int, event_city=$9::text, event_state=$10::text, event_zip=$11:int, event_end_time=$12::time, event_start_time=$13::time where id=$14::int",
       [
         req.body.event_name,
         req.body.event_address,
-        req.body.event_time,
         req.body.event_date,
         req.body.event_notes,
+        req.body.starting_address,
+        req.body.starting_city,
+        req.body.starting_state,
+        req.body.starting_zip,
+        req.body.event_city,
+        req.body.event_state,
+        req.body.event_zip,
+        req.body.event_end_time,
+        req.body.event_start_time,
         req.params.id
       ]
     )
--- routes/weather.js
+++ routes/weather.js
@@ -3,14 +3,18 @@ const weatherRouter = express.Router();
 const axios = require("axios");
 
 weatherRouter.get("/weather", (req, res) => {
-    const { zip } = req.query;
-    axios
-      .get(
-        `http://api.openweathermap.org/data/2.5/weather?zip=${zip},us&APPID=497f7f87f95db03697266084d1926586`
-      )
-      .then(response => {
-        res.send(response.data);
-      });
-  });
+  const { zip } = req.query;
+  axios
+    .get(
+      `http://api.openweathermap.org/data/2.5/weather?zip=${zip},us&APPID=497f7f87f95db03697266084d1926586`
+    )
+    .then(response => {
+      res.send(response.data);
+    })
+    .catch(e => {
+      console.log(e);
+      res.send(e);
+    });
+});
 
 module.exports = weatherRouter;
