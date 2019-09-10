ff3ccf6583b7cbff1726b4ae29a9f7b0f8139b14
Hillary <uptonh13@gmail.com>
2019-09-10 09:29:42 -0400

    Set up proxy server for weather api

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ const app = express();
 const toDoRouter = require("./routes/todo.api");
 const eventRouter = require("./routes/event.api");
 const mapRouter = require("./routes/map");
+const weatherRouter = require("./routes/weather");
 
 app.use(cors());
 app.use(express.json());
 app.use("/", toDoRouter);
 app.use("/", eventRouter);
 app.use("/", mapRouter);
+app.use("/", weatherRouter);
 
 const port = 5252;
 app.listen(port, () => console.log(`Server is running on PORT: ${port}.`));
